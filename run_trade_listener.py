4163ad33439e1761f1eef245c27c964cc6c67112
Frankie Guapo <frankie.guapo@gmail.com>
2023-08-14 21:26:02 -0500

    Added logging

--- run_trade_listener.py
+++ run_trade_listener.py
@@ -2,9 +2,11 @@
 
 import os
 import sys
+import logging
 
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 
+from regex_functionality import is_nas_trade
 
 def assert_api_id():
     try:
@@ -24,10 +26,46 @@ def assert_api_hash():
 
 
 
+def run_trader():
+    assert_api_id()
+    assert_api_hash()
+
+    api_id = os.environ['API_ID']
+    api_hash = os.environ['API_HASH']
+
+    client = TelegramClient('listener', api_id, api_hash)
+
+    @client.on(events.NewMessage)
+    async def new_message_handler(event):
+        # Change logic to if related to NAS100 trade, add printout to log file
+        message_raw_text = event.raw_text
+        logging.info('New Message Received')
+        logging.info('Message Raw Text: %s', message_raw_text)
+
+        print('New Message Received!')
+
+        parsed_dict = is_nas_trade(message_raw_text)
+
+        if not parsed_dict:
+            logging.info("Message Not Classified as NAS100 Trade")
+
+        elif parsed_dict:
+            logging.info("Message Parsed as: %s", parsed_dict.__repr__())
+        
+    client.start()
+    print("Telethon client started!")
+    client.run_until_disconnected()
+
+
+
+
 
 
 if __name__ == "__main__":
-    assert_api_id()
-    assert_api_hash()
+    logging.basicConfig(filename='listener.log',
+                    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+    
+    run_trader()
 
 
